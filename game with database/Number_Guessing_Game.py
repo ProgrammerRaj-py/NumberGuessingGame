77f6b09d3a55074ea0aa8939dd8527796859cc5a
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2020-05-13 12:33:52 +0530

    Update Number_Guessing_Game.py
    update last game played last score etc.

--- game with database/Number_Guessing_Game.py
+++ game with database/Number_Guessing_Game.py
@@ -3,37 +3,55 @@ import random as r
 
 # game
 def game():
-    winning_number = 55
-    user_guessed = int(input('guess the number : '))
-    turn = 1000
-
+    user_said = input(f'{user_name} do you want to play (Y/N) : ').lower()
+    turn2 = 0
+    y_said = 0
     while True:
-        if turn == 0:
-            print(f'No guesses left. The number is {winning_number}.')
+
+        if ('y' not in user_said) and ('n' not in user_said) and (user_said != True):
+            user_said = input('Invalid keyword. Type again : ')
+
+        elif 'y' in user_said:
+            print('Let\'s play.Good luck!\n')
+            winning_number = r.randint(1,100)
+            user_guessed = int(input('guess the number : '))
+            turn = 1000   
+
+            while True:
+                if turn == 0:
+                    print(f'No guesses left. The number is {winning_number}.')
+                    break
+
+                elif winning_number == user_guessed:
+                    print('congrats you guess the number')
+                    break
+                else:
+                    if winning_number > user_guessed:
+                        print('Too low')
+                    else:
+                        print('Too high')
+                    turn -= 100
+                    print(f'{turn} points left.')
+                
+                if turn != 0:
+                    user_guessed = int(input('Guess again : '))
+                else:
+                    continue
+            print(f'Score = {turn}\n')
+            user_said = input(f'{user_name} do you want to play more (Y/N) : ')
+            turn2 += turn
+            y_said += 1
+
+        else:
             break
 
-        elif winning_number == user_guessed:
-            print('congrats you guess the number')
-            break
-        else:
-            if winning_number > user_guessed:
-                print('Too low')
-            else:
-                print('Too high')
-            turn -= 100
-        
-        if turn != 0:
-            user_guessed = int(input('Guess again : '))
-        else:
-            continue
-
-    return turn
+    return turn2, y_said
 
 
 # update data 
-def update_data(user_name,score):
+def update_data(user_name,score,game_played):
     update_new_data = open('database.txt', 'a')
-    update_new_data.write(f'\n{user_name},{score}')
+    update_new_data.write(f'\n{user_name},{score},{game_played}')
     update_new_data.close()
 
 
@@ -45,16 +63,16 @@ data_file = open('database.txt', 'r')
 
 # old data create as a dict
 data_names  = {}
-for names in data_file.readlines():
-    name,score = names.split(',')
-    data_names[name] = score
-
-
-# New data create as a list
+# new data
 data_names2 = []
+data_game_play = [] # for finding last gameplayed
 for names in data_file.readlines():
-    names,score = names.split(',')
-    data_names2.append(names)
+    name,score,g = names.split(',')
+    data_names[name] = score
+    data_names2.append(name)
+    
+    if user_name in name:
+        data_game_play.append(int(g))
 
 # close the file
 data_file.close()
@@ -62,22 +80,22 @@ data_file.close()
 # check if user name is already in database or not
     # if True 
 if user_name in data_names:
-    print(f'Welcome back {user_name}.\nYour last score was {data_names[user_name]}\nYou have 10 chance & 1000 points. You know the rules.\nEvery wrong guess -100 points.\nLet\'s start.\n')
-    score = game()
-    print(f'Your new score is {score} points.')
+    print(f'\nWelcome back {user_name}.\nYour last score was {data_names[user_name]} in {data_game_play[-1]} games.\nYou know the rules 10 chance & 1000 points.\n')
+    score, game_played = game()
+    print(f'You scored {score} points in {game_played} games.')
+    print(f'Ok bye {user_name}. See you soon')
 
     # update data
-    update_data(user_name,score)
+    update_data(user_name,score,game_played)
 
     # if False
 else:
     data_names2.append(user_name)
-    print(f'Welcome {user_name}.\nYou have 10 & 1000 points chance to guess the number.\nYou should guess the number between 1 and 100.\nEvery wrong guess -100 points.\nLet\'s play.Good luck!\n')
-    score = game()
-    print(f'Your new score is {score} points.')
+    print(f'\nWelcome {user_name}.\nYou have 1000 points & 10 chance to guess the number.\nYou should guess the number between 1 and 100.\nEvery wrong guess -100 points.\n')
+    score, game_played = game()
+    print(f'You scored {score} points in {game_played} games.')
+    print(f'Ok bye {user_name}. See you soon')
+    
 
     # Update data
-    update_data(user_name,score)
-
-
-
+    update_data(user_name,score,game_played)
